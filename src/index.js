90a9eac6a9e7558630951b1b89ee4311788a0f79
Martin Packman <martin.packman@visual-meaning.com>
2020-11-14 17:07:51 +0000

    Support Webpack 5 method of adding an asset
    Fixes microsoft/license-checker-webpack-plugin#27
    
    Changes based on dialogue in webpack/webpack#11425 between webpack and
    workbook-webpack-plugin devs.
    
    Split tap and asset logic in `LicenseCheckerWebpackPlugin.apply()` by
    version of webpack. Use PROCESS_ASSETS_STAGE_ADDITIONAL for webpack 5.
    Keep existing logic for webpack 4 largely unchanged.
    
    Update peerDependencies to `^4.4.0 || ^5.4.0`.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const { RawSource } = require("webpack-sources");
+const webpack = require("webpack");
+const { RawSource } = webpack.sources || require("webpack-sources");
 const {
   getLicenseInformationForCompilation,
   getLicenseViolations,
@@ -9,40 +10,51 @@ const {
 } = require("./licenseUtils");
 const { getOptions } = require("./optionsUtils");
 
+async function buildAsset(compilation, options) {
+  const { filter, allow, ignore, override, emitError, outputFilename, outputWriter } = options;
+
+  let licenseInformation = getLicenseInformationForCompilation(compilation, filter);
+  licenseInformation = ignoreLicenses(licenseInformation, ignore);
+  licenseInformation = overrideLicenses(licenseInformation, override);
+
+  const licenseViolations = getLicenseViolations(licenseInformation, allow);
+  if (emitError) {
+    compilation.errors.push(...licenseViolations);
+  } else {
+    compilation.warnings.push(...licenseViolations);
+  }
+
+  const sortedLicenseInformation = getSortedLicenseInformation(licenseInformation);
+  return [
+    outputFilename,
+    new RawSource(await writeLicenseInformation(outputWriter, sortedLicenseInformation))
+  ];
+}
+
 class LicenseCheckerWebpackPlugin {
   constructor(options) {
     this.options = getOptions(options);
   }
 
   apply(compiler) {
-    const {
-      filter,
-      allow,
-      ignore,
-      override,
-      emitError,
-      outputFilename,
-      outputWriter
-    } = this.options;
-
-    compiler.hooks.emit.tapPromise("LicenseCheckerWebpackPlugin", async compilation => {
-      let licenseInformation = getLicenseInformationForCompilation(compilation, filter);
-      licenseInformation = ignoreLicenses(licenseInformation, ignore);
-      licenseInformation = overrideLicenses(licenseInformation, override);
-
-      const licenseViolations = getLicenseViolations(licenseInformation, allow);
-      if (emitError) {
-        compilation.errors.push(...licenseViolations);
-      } else {
-        compilation.warnings.push(...licenseViolations);
-      }
-
-      const sortedLicenseInformation = getSortedLicenseInformation(licenseInformation);
-      // eslint-disable-next-line require-atomic-updates
-      compilation.assets[outputFilename] = new RawSource(
-        await writeLicenseInformation(outputWriter, sortedLicenseInformation)
-      );
-    });
+    const { name } = this.constructor;
+    if (webpack.version.startsWith("4.")) {
+      compiler.hooks.emit.tapPromise(name, async compilation => {
+        const [filename, source] = await buildAsset(compilation, this.options);
+        compilation.assets[filename] = source;
+      });
+    } else {
+      compiler.hooks.thisCompilation.tap(name, compilation => {
+        const hook = {
+          name,
+          stage: webpack.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL
+        };
+        compilation.hooks.processAssets.tapPromise(hook, async () => {
+          const [filename, source] = await buildAsset(compilation, this.options);
+          compilation.emitAsset(filename, source);
+        });
+      });
+    }
   }
 }
 
