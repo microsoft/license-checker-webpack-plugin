575de08a5eb71f48348d4d3414223601185cf03d
Martin Packman <martin.packman@visual-meaning.com>
2021-04-04 03:12:52 +0100

    Filter scoped modules correctly
    Fixes microsoft/license-checker-webpack-plugin#25
    
    A valid node module name is either `\w+` or `@\w+/\w+` never `@\w+`.
    
    This is enough of a hairy internal implementation detail that filter
    should probably not be exposed even as an (undocumented) option. But
    leaving alone for now as changing the pattern has been a useful
    workaround.

--- src/optionsUtils.js
+++ src/optionsUtils.js
@@ -32,7 +32,7 @@ const optionsSchema = object({
 });
 
 const defaultOptions = {
-  filter: /(^.*[/\\]node_modules[/\\]((?:@[^/\\]+[/\\])?(?:[^/\\]+)))/,
+  filter: /(^.*[/\\]node_modules[/\\]((?:@[^/\\]+[/\\])?(?:[^@/\\][^/\\]*)))/,
   allow: "(Apache-2.0 OR BSD-2-Clause OR BSD-3-Clause OR MIT)",
   ignore: [],
   override: {},
